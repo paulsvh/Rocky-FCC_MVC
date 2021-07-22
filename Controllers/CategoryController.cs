0a8b10c6ca43cdec10e155df546383ac33beaa4d
paulsvh <pvonhagen26@gmail.com>
2021-07-22 16:24:02 -0700

    Show table of categories by name and display order in category index view

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,16 @@ namespace Rocky.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
         }
     }
 }
